5e79941b5a1b8028f20932b7d213336e6abd4a60
Diego Sanchez <diegoroj3@gmail.com>
2021-04-01 22:10:48 -0500

    Creacion de apps e index
    se creo la aplicacion django con los respectivos modulos

--- aeropuerto-proyect/aeropuerto/settings.py
+++ aeropuerto-proyect/aeropuerto/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vuelos',
+    'clientes',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,STATIC_URL)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,MEDIA_URL)
+
