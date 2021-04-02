c5194468bfbe755ecc8e4f80b5eb0f89eccb06f1
Diego Sanchez <diegoroj3@gmail.com>
2021-04-02 16:38:44 -0500

    correccion rutas y merge

--- aeropuerto-proyect/clientes/views.py
+++ aeropuerto-proyect/clientes/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout,authenticate
 from django.contrib.auth.models import  User
-<<<<<<< HEAD
 from django.http import  HttpResponse
 from forms import Loginform, Aerolineaform, Vueloform
 #
-=======
-from forms import Loginform, Aerolineaform, RegistroClienteform 
-from django.http import  HttpResponse
->>>>>>> 273588fc477c883c9ccd64366a209f809a192a61
 # Create your views here.
 def login(request):
     form = Loginform()
@@ -29,8 +24,6 @@ def login(request):
             context['error'] = 'Usuario no registrado'
             return render(request,'vuelos/login.html',context)
 
-<<<<<<< HEAD
-=======
 def registrar_usuario(request):
     form = RegistroClienteform()
 
@@ -38,4 +31,3 @@ def registrar_usuario(request):
         return HttpResponse(request.POST['cc'] + " " + request.POST['username']+ " " + request.POST['email']+ " " + request.POST['password1']+ " " + request.POST['password2'])
     else:
         return render(request,'vuelos/signup.html',{'form':form})
->>>>>>> 273588fc477c883c9ccd64366a209f809a192a61
