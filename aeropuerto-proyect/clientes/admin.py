1e1b54f6a85a33957e51fbca6ba3acacd5331397
Yahir Eduardo Bravo <yahir.bravo@correounivalle.edu.co>
2021-04-02 11:26:10 -0500

    reparacion de errores en los modelos

--- aeropuerto-proyect/clientes/admin.py
+++ aeropuerto-proyect/clientes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Cliente, Reserva
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    fields = ('cc','vuelos_disponibles','usuario_dj')
+
+class ReservaAdmin(admin.ModelAdmin):
+    fields = ('cliente','vuelo')
+
+
+admin.site.register(Cliente,ClienteAdmin)
+admin.site.register(Reserva,ReservaAdmin)
--- aeropuerto-proyect/vuelos/models.py
+++ aeropuerto-proyect/vuelos/models.py
@@ -4,19 +4,19 @@ from django.contrib.auth.models import User
 # MODELOS
 
 class Aerolinea(models.Model):
-    nombre = models.CharField(max) 
+    nombre = models.CharField(max_length=100) 
 
 class Vuelo(models.Model):
     id_vuelo = models.CharField(max_length=30,primary_key=True)
     aerolinea = models.ForeignKey(Aerolinea,on_delete=models.CASCADE)
-    costo = models.DecimalField(max_digits=30, blank=False, null=False )
+    costo = models.FloatField(blank=False,null=False)
 
 class Escala(models.Model):
     vuelo = models.ForeignKey(Vuelo,on_delete=models.CASCADE)
     nombre_procedencia = models.CharField(max_length=30,null=False,blank=False)
-    cod_procedencia = models.IntegerField(max_length=30,null=False,blank=False)
+    cod_procedencia = models.IntegerField(null=False,blank=False)
     nombre_destino = models.CharField(max_length=30,null=False,blank=False)
-    cod_destino = models.IntegerField(max_length=30,null=False,blank=False)
+    cod_destino = models.IntegerField(null=False,blank=False)
     fecha_salida = models.DateTimeField(null=False,blank=False)
     fecha_llegada = models.DateTimeField(null=False,blank=False)
     requicitos = models.TextField(null=False,blank=False)
