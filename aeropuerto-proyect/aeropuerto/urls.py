5c03436656f083b1b82a3e16d622628a5b3c67fe
Yahir Eduardo Bravo <yahir.bravo@correounivalle.edu.co>
2021-04-02 16:39:42 -0500

    -m

--- aeropuerto-proyect/aeropuerto/urls.py
+++ aeropuerto-proyect/aeropuerto/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('vuelos.urls'))
+    path('',include('vuelos.urls')),
+    path('',include('clientes.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
