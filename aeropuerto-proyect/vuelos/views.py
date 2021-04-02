c91f562559afda74583a6f5667c313be82eb5910
Daniel Cossio <daniel.cossio@correounivalle.edu.co>
2021-04-02 12:01:36 -0500

    Agregado el html de registrar cliente

--- aeropuerto-proyect/vuelos/views.py
+++ aeropuerto-proyect/vuelos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout,authenticate
 from django.contrib.auth.models import  User
-from forms import Loginform
+from forms import Loginform, SignUpForm
 # Create your views here.
 def index(request):
     return render(request,'vuelos/index.html')
