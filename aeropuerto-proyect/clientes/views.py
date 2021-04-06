818bf033fac1da18354e1de751fee0a02263323c
Diego Sanchez <diegoroj3@gmail.com>
2021-04-06 18:40:30 -0500

    ARREGLANDO ERROR EN REGISTRAR
    No cambiar el registrar el formulario de manera automatica no registra
    tanto cliente como usuario dejar, la solucion ya establecida.

--- aeropuerto-proyect/clientes/views.py
+++ aeropuerto-proyect/clientes/views.py
@@ -31,26 +31,7 @@ def logout_user(request):
     return redirect('/')
 
 def registrar_usuario(request):
-    if request.method == 'POST':
-        form = RegistroClienteform(request.POST)
-        if form.is_valid():
-            # registrar los usuarios y el cliente
-            usr = User(username=request.POST["username"], email=request.POST["email"], password=request.POST["password1"])
-            # cerar usuario en la base de datos
-            usr.save()
-            #crear cliente
-            clt = Cliente(cc=request.POST["cc"],vuelos_disponibles=2, usuario_dj_id=usr.id)
-            clt.save()
-            return redirect('clientes:login')
-        else:
-            return render(request,'vuelos/signup.html',{'form':form})
-    else:
-        form = RegistroClienteform()
-        return render(request,'vuelos/signup.html',{'form':form})
-
-
-
-"""
+    
     if request.method == 'POST':
         p1 = request.POST['password1']
         p2 = request.POST['password2']
@@ -78,7 +59,6 @@ def registrar_usuario(request):
             return render(request,'vuelos/signup.html',context)
     else:
         return render(request,'vuelos/signup.html',{'form':form})
-"""
 
 def perfil(request):
     return render(request, 'vuelos/perfil.html')
